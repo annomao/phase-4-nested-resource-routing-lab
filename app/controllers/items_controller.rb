74db6ecc38efaf64edcc5c64558b767d35b973f0
annomao <omaoannkerubo@gmail.com>
2022-07-22 23:44:40 +0300

    commit tree 6db0123a09de076f8a6ed9d7c20825d723a5fe68

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(allowed_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def not_found_response
+    render json: {error: "record not found"}, status: :not_found
+  end
+
+  def allowed_params
+    params.permit(:name, :description, :price)
   end
 
 end
